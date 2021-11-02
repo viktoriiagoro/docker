81bb987e36d40cdcf75b54910b64b1e7e5792b23
Viktoriia Gorodetska <vikagoro3@gmail.com>
2021-11-02 17:13:13 +0200

    create_db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-const { app } = require("express")
-
 const {port, host} = require('./configuration')
 const {connectDB} = require('./helpers/db')
+const bodyParser = require('body-parser')
+const {User} = require('./models/user')
+const express = require('express')
+
+const app = express()
 
 connectDB(err => {
     if (err) console.error(err)
@@ -11,5 +14,38 @@ connectDB(err => {
     })
 })
 
-app.get('/healthtech', (req, res) => res.status(200))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/healthcheck', (req, res) => res.status(200).json({ isOk: true }))
+
+// create user
+app.post('/users', async (req, res) => {
+    try {
+        const { firstName, lastName } = req.body
+        if (!firstName || !lastName) {
+            res.status(400).send('Bad Request')
+            return
+        }
+        const user = new User({ firstName, lastName })
+        await user.save()
+        res.status(201).json(user)
+    } catch (err){
+        res.send({err})
+    }
+
+})
+
+// create all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (err){
+        res.send({err})
+    }
+
+})
---
+++ api/models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+const User = mongoose.model('User', userSchema);
+module.exports.User = User
